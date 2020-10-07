5e2feb2d4704fccf03990f2bec59d47789df35e3
jesua <jesua@dam100>
2020-10-07 09:46:38 +0200

    5

---
+++ 21_Condicionales/src/_5_ClaveCorrecta.java
@@ -0,0 +1,26 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class _5_ClaveCorrecta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner entrada=new Scanner(System.in);
+		Console consola=null;		
+		consola=System.console();
+		char[] clave=null;
+		String nombre=null;
+		if (consola != null) {
+			nombre = consola.readLine("Introduce tu nombre: ");
+			clave=consola.readPassword("Introduce la clave: ");
+		 }
+		String claves=String.valueOf(clave);
+		System.out.println("nombre:"+nombre);
+		System.out.println("clave:"+claves);
+		if (nombre=="DAM1" && claves=="JaVa")
+			System.out.println("Usuario autorizado");
+		else
+			System.out.println("Usuario no autorizado");
+	}
+
+}
