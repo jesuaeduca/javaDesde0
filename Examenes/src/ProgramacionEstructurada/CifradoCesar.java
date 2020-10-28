486ed91e380b69e0d1a664240f1517a68c5f49bf
jesua <jesua@Thiar>
2020-10-28 20:14:03 +0100

    cesar

--- Examenes/src/ProgramacionEstructurada/CifradoCesar.java
+++ Examenes/src/ProgramacionEstructurada/CifradoCesar.java
@@ -1,5 +1,7 @@
 package ProgramacionEstructurada;
 
+import java.util.Scanner;
+
 /*
  * Crea un  programa que  sirva  para  cifrar  un  texto  con  el  conocido  
  * método  de  César. El criptosistema consiste en el desplazamiento de 3 caracteres 
@@ -9,7 +11,7 @@ package ProgramacionEstructurada;
  * solo contiene caracteres alfabéticos en mayúscula.
  * 
  * Realiza un programa que lea una cadena de texto por teclado y muestre el texto cifrado.
- * No se deben utilizar condicionales 
+ * No se puede utilizar condicionales.
  * 
  */
 
@@ -17,7 +19,16 @@ public class CifradoCesar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String alfabeto="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		System.out.print("Introduce la cadena de texto a cifrar: ");
+		Scanner scan=new Scanner(System.in);
+		String cadena=scan.nextLine();
+		String cadenaCifrada="";
+		for (int i=0;i<cadena.length();i++) {
+			cadenaCifrada+=alfabeto.charAt((alfabeto.indexOf(cadena.charAt(i))+3)%27);
+		}
+		System.out.println("Cadena cifrada: "+cadenaCifrada);
+		scan.close();
 	}
 
 }
