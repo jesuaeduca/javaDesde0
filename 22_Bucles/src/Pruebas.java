4abd8e96785871a304e8199a9e6d711a6054376b
jesua <jesua@dam100>
2020-11-04 09:45:24 +0100

    pruebas

--- 22_Bucles/src/Pruebas.java
+++ 22_Bucles/src/Pruebas.java
@@ -1,26 +1,36 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
+/*
+ * Vamos a hacer un programa para jugar a adivinar un número.
+
+    El programa elegirá un número al azar entre el 1 y el 999
+    Hay que intentar adivinar ese número en un número máximo de intentos. 
+    El número máximo de intentos se preguntará al inicio del juego.
+    Se podrá intentar adivinar el número hasta que queden intentos. 
+    El programa mostrará cuantos intentos quedan, y al introducir el número mostrará si has 
+    acertado o no.
+    En caso de no acertar, el juego dará una pista para ayudarnos a encontrar el número.
+
+ */
 public class Pruebas {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-        System.out.print("Introduzca numero de filas: ");
-        int num = input.nextInt();
-        System.out.print("Introduzca el caracter: ");
-        String cara = input.next();
-         
-        int cont = 0;
-                 
-        for(int i=0; i<(num+1); i++ ){
-        	
-        	for(int x=1;x<=cont;x++){
-                System.out.print(cara);
-            }
-        	cont++;
-            System.out.println();
-        }
-        
-        input.close();
-	}
 
+		int numeroSecreto=(int) (Math.random()*999+1);
+		int numeroIntentos=Integer.parseInt(JOptionPane.showInputDialog("Introduce cuantos intentos quieres"));
+		while (numeroIntentos>0) {
+			int numero=Integer.parseInt(JOptionPane.showInputDialog("¿Cual crees que es el número? Te quedan "+numeroIntentos+" intentos"));			
+			if (numero>numeroSecreto)
+				System.out.println("El número que has elegido es mayor que el secreto");
+			else if (numero<numeroSecreto)
+				System.out.println("El número que has elegido es menor que el secreto");
+			else
+				break;
+			numeroIntentos--;
+		}
+		if (numeroIntentos==0)
+			System.out.println("Lo siento, no has adivinado el número, era el "+numeroSecreto);
+		else
+			System.out.println("Enhorabuena, has acertado el número");
+	}
 }
